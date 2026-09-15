82d8d28e47ba78da5f0c268cb7ecaf2d951a8859
kiran30422 <vtu30422@veltech.edu.in>
2026-09-15 11:07:43 +0530

    Add Richest Customer Wealth solution
    Implemented a solution to calculate the maximum wealth among customers based on their bank accounts.

---
+++ SESSION 4/Richest Customer Wealth.java
@@ -0,0 +1,21 @@
+#PROGRAM
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for (int[] customer : accounts) {
+            int currentWealth = 0;
+            for (int bank : customer) {
+                currentWealth += bank;
+            }
+            maxWealth = Math.max(maxWealth, currentWealth);
+        }
+        return maxWealth;
+    }
+}
+
+#INPUT
+accounts =
+[[1,2,3],[3,2,1]]
+
+#OUTPUT
+6
