eb847c60115ff022adaeafbc8a5bdfaa0e91908d
kiran30422 <vtu30422@veltech.edu.in>
2026-09-15 11:09:40 +0530

    Add Find Pivot Index solution
    Implement pivot index calculation in a new class.

---
+++ SESSION 5/Find Pivot Index.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+
+        // Calculate total sum of the array
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        // Check each index
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
+input
+nums =
+[1,7,3,6,5,6]
+Output
+3
